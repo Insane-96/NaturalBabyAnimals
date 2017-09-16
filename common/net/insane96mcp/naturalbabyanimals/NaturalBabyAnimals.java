2afe3f7f362fbb8d23431cf98e102467a3e59fb3
Alberto Del Villano <insane96mcp@gmail.com>
2017-09-16 11:00:16 +0200

    1.0.1

--- common/net/insane96mcp/naturalbabyanimals/NaturalBabyAnimals.java
+++ common/net/insane96mcp/naturalbabyanimals/NaturalBabyAnimals.java
@@ -15,7 +15,7 @@ public class NaturalBabyAnimals {
 	
 	public static final String MOD_ID = "naturalbabyanimals";
 	public static final String MOD_NAME = "Natural Baby Animals";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
 
 	public static Random random = new Random();
