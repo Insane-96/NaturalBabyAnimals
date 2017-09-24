637ded7f2524798136146b036e02c3785699dc6e
Alberto Del Villano <insane96mcp@gmail.com>
2017-09-24 08:50:03 +0200

    Added support for BabyMobs.

--- common/net/insane96mcp/naturalbabyanimals/lib/Stats.java
+++ common/net/insane96mcp/naturalbabyanimals/lib/Stats.java
@@ -7,9 +7,13 @@ public class Stats {
 	public static int maxAgeTicks;
 	public static float chance;
 	
+	public static boolean babyMobs;
+	
 	public static void Init() {
 		minAgeTicks = Config.LoadIntProperty("general", "min_age_ticks", "Minium random value to set the baby animals age", 6000);
 		maxAgeTicks = Config.LoadIntProperty("general", "max_age_ticks", "Maximum random value to set the baby animals age", 24000);
 		chance = Config.LoadFloatProperty("general", "chance", "Chance for an animal to become a baby animal", 50f);
+		
+		babyMobs = Config.LoadBoolProperty("mod_support", "baby_mobs", "Whenever the Zombie Chicken should be made baby in the Baby Mobs mod", false);
 	}
 }
