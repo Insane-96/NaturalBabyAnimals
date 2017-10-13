a2e0d0c02e9b2ccec49b8bd4c2cd2028ec141ed7
Alberto Del Villano <insane96mcp@gmail.com>
2017-10-13 17:21:31 +0200

    1.0.4

--- common/net/insane96mcp/naturalbabyanimals/NaturalBabyAnimals.java
+++ common/net/insane96mcp/naturalbabyanimals/NaturalBabyAnimals.java
@@ -15,7 +15,7 @@ public class NaturalBabyAnimals {
 	
 	public static final String MOD_ID = "naturalbabyanimals";
 	public static final String MOD_NAME = "Natural Baby Animals";
-	public static final String VERSION = "1.0.3";
+	public static final String VERSION = "1.0.4";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
 	
 	@Instance(MOD_ID)
