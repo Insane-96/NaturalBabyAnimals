9c0f7d1be75e697e62ff247093df382fecbabdbe
Alberto Del Villano <insane96mcp@gmail.com>
2017-11-14 08:50:57 +0100

    1.1.0b, updated Forge and Mappings

--- common/net/insane96mcp/naturalbabyanimals/NaturalBabyAnimals.java
+++ common/net/insane96mcp/naturalbabyanimals/NaturalBabyAnimals.java
@@ -16,7 +16,7 @@ public class NaturalBabyAnimals {
 	
 	public static final String MOD_ID = "naturalbabyanimals";
 	public static final String MOD_NAME = "Natural Baby Animals";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.0b";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
 	
 	@Instance(MOD_ID)
