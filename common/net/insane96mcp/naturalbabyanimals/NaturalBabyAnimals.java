704c23d0555877a149b14ef2ecc82a0f176da39f
Alberto Del Villano <insane96mcp@gmail.com>
2017-11-18 23:04:30 +0100

    1.1.2. Mod can now run on server side only

--- common/net/insane96mcp/naturalbabyanimals/NaturalBabyAnimals.java
+++ common/net/insane96mcp/naturalbabyanimals/NaturalBabyAnimals.java
@@ -11,18 +11,18 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = NaturalBabyAnimals.MOD_ID, name = NaturalBabyAnimals.MOD_NAME, version = NaturalBabyAnimals.VERSION)
+@Mod(modid = NaturalBabyAnimals.MOD_ID, name = NaturalBabyAnimals.MOD_NAME, version = NaturalBabyAnimals.VERSION, acceptableRemoteVersions = "*")
 public class NaturalBabyAnimals {
 	
 	public static final String MOD_ID = "naturalbabyanimals";
 	public static final String MOD_NAME = "Natural Baby Animals";
-	public static final String VERSION = "1.1.1";
+	public static final String VERSION = "1.1.2";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
 	
 	@Instance(MOD_ID)
 	public static NaturalBabyAnimals instance;
 	
-	@SidedProxy(clientSide = "net.insane96mcp.naturalbabyanimals.proxies.ClientProxy", serverSide = "net.insane96mcp.naturalbabyanimals.proxies.ServerProxy")
+	@SidedProxy(clientSide = "net.insane96mcp.naturalbabyanimals.wproxies.ClientProxy", serverSide = "net.insane96mcp.naturalbabyanimals.proxies.ServerProxy")
 	public static CommonProxy proxy;
 	
 	@EventHandler
