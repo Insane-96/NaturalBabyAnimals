d673fb0f8b9014cb624187cd6873985de28bb54c
Alberto Del Villano <insane96mcp@gmail.com>
2017-11-19 10:24:50 +0100

    1.1.3. Mod can now run on every 1.12 version

--- common/net/insane96mcp/naturalbabyanimals/NaturalBabyAnimals.java
+++ common/net/insane96mcp/naturalbabyanimals/NaturalBabyAnimals.java
@@ -11,13 +11,14 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = NaturalBabyAnimals.MOD_ID, name = NaturalBabyAnimals.MOD_NAME, version = NaturalBabyAnimals.VERSION, acceptableRemoteVersions = "*")
+@Mod(modid = NaturalBabyAnimals.MOD_ID, name = NaturalBabyAnimals.MOD_NAME, version = NaturalBabyAnimals.VERSION, acceptableRemoteVersions = "*", acceptedMinecraftVersions = NaturalBabyAnimals.MINECRAFT_VERSIONS)
 public class NaturalBabyAnimals {
 	
 	public static final String MOD_ID = "naturalbabyanimals";
 	public static final String MOD_NAME = "Natural Baby Animals";
-	public static final String VERSION = "1.1.2";
+	public static final String VERSION = "1.1.3";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
+	public static final String MINECRAFT_VERSIONS = "[1.12,1.12.2]";
 	
 	@Instance(MOD_ID)
 	public static NaturalBabyAnimals instance;
